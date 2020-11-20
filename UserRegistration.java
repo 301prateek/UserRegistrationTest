5d16b17166b38659307b4d5fad278dbaba03ce15
301prateek <prateek.patil301@gmail.com>
2020-11-20 16:45:17 +0530

    Added: First Name Pattern

---
+++ UserRegistration.java
@@ -0,0 +1,16 @@
+package com.uservalidation.main;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public boolean validateFirstName(String name) {
+		String expression = "^[A-Z]{1}[a-z]{2,}$";
+
+		if(name.matches(expression))
+			return true;
+		else
+			return false;
+	}
+}
